aad942e878f45a01662021ba6c8435e658a43f5b
nuka10933 <131746481+nuka10933@users.noreply.github.com>
2023-05-08 22:41:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -48,6 +48,7 @@ const config = Object.freeze({
   SERPAPI_API_KEY: env.SERPAPI_API_KEY || null,
   SERPAPI_LOCATION: env.SERPAPI_LOCATION || 'Taiwan',
   SERPAPI_LANG: env.SERPAPI_LANG || 'lang_zh-TW',
+  GOOGLE_MAPS: env.GOOGLE_MAPS || null,
 });
 
 export default config;
