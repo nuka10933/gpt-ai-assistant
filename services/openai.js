72264b15202a944838672d95c1cda6b2f8dc0965
nuka10933 <131746481+nuka10933@users.noreply.github.com>
2023-05-09 17:02:23 +0800

    Fix utils

--- services/openai.js
+++ services/openai.js
@@ -99,23 +99,6 @@ const createAudioTranscriptions = ({
   });
 };
 
-const { Configuration, OpenAIApi } = require("openai");
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-const response = await openai.createCompletion({
-  model: "davinci:ft-personal:custom-model-name-2023-05-08-16-55-58",
-  prompt: "",
-  temperature: 0.7,
-  max_tokens: 256,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-});
-
 export {
   createChatCompletion,
   createTextCompletion,
